a6183d1ea85aab64dd70ee1ca6f518bc4c16ffce
ali.abukahil <ali.abukahil@gmail.com>
2021-04-26 15:54:40 +0200

    Updates

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
 console.log("Hi There Amigos");
+
+//Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+// Event Listeners
+
+todoButton.addEventListener("click", addTodo);
+
+//Functions
+
+function addTodo(event) {
+  // to prevent form from submitting.
+  event.preventDefault();
+  //Todo Div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  // Create LI
+  const newTodo = document.createElement("li");
+  newTodo.innerText = "おはようございます";
+  newTodo.classList.add("todo-item");
+}
